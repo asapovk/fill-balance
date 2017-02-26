705022e9003a289c74abcc145c8126ef50cd779b
asapovk <asapovk@gmail.com>
2017-02-26 15:23:23 +0300

    accept code

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,13 @@
 exports.get = async (ctx) => {
-  ctx.body = ctx.render('templates/home');
+  try {
+     var code = ctx.request.query.code;
+  } catch(e) {}
+  if (code == undefined ) {
+    ctx.body = ctx.render('templates/home');
+  }
+  else {
+    console.log(code);
+    ctx.body = ctx.render('templates/home');
+  }
+
 }
