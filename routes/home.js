4a374fbada7dc4fa710cf9604603e4996031d303
asapovk <asapovk@gmail.com>
2017-02-27 23:40:24 +0300

    request functionalyty on servers side has started

--- routes/home.js
+++ routes/home.js
@@ -1,3 +1,6 @@
+var rp = require('request-promise');
+
+
 exports.get = async (ctx) => {
   try {
      var code = ctx.request.query.code;
@@ -7,6 +10,18 @@ exports.get = async (ctx) => {
   }
   else {
     console.log(code);
+    var options = {
+      method: 'GET',
+      uri: 'https://oauth.vk.com/access_token',
+      qs: {
+        client_id: '5895475',
+        client_secret: 'CWWUwXFXOOw1UCJXIjef',
+        redirect_uri: 'http://oknojapan.com/',
+        code: code
+      },
+      json: true
+    }
+    await rp(options).then(respose => {console.log(respose)});
     ctx.body = ctx.render('templates/home');
   }
 
