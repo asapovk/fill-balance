5280f97c952fe35b7d5e9e7676c902bd22d5ad99
asapovk <asapovk@gmail.com>
2017-02-28 15:38:15 +0300

    again

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,6 @@ exports.get = async (ctx) => {
 
     var user = ctx.session.user || null;
 
-    console.log(user);  
-    ctx.body = ctx.render('templates/home', {user: user});
+    console.log(user);
+    ctx.body = ctx.render('templates/home', {user12: user});
 }
